87a6fcec074dc4220ed6710de3a8f49f2be5700b
khanhnvm <minhbi245@gmail.com>
2024-09-06 14:59:02 +0700

    create code demo with Playground

---
+++ Swift/source-code/060924/variable.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+var numberIneger: Int = 252
+var numberFloat: Float = 5.2
+var numberDouble: Double = 25.2552
+let myName: String = "KhanhNVM"
+var isCorrect: Bool = true // Data type Boolean
+
+func sayHello() {
+    print("Hello \(myName)")
+}
+
+func printOut() {
+    print(numberIneger)
+    print(numberFloat)
+    print(numberDouble)
+}
+
+// Call function
+sayHello()
+printOut()
