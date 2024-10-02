fa0e7040cebc131776ff4dcd8b907195895b606d
khanhnvm <minhbi245@gmail.com>
2024-10-03 00:33:50 +0700

    create the first basic 1 learn from Allen Hong Inflearn

---
+++ iOS/basic-one-alleniOS/basic-one-alleniOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  basic-one-alleniOS
+//
+//  Created by khanhnvm on 24/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
